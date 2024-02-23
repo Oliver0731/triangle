7fb4fb559c34cced8113cc8faa0c9e66b712ee23
Oliver0731 <o.townell@share.epsb.ca>
2024-02-23 09:16:57 -0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+//eventlistener
+document.getElementById("btn").addEventListener("click", dist)
+
+let xA = id("xA");
+
+
+
+
 // Triangle Perimeter Assignment Start Code
 function dist(x1, y1, x2, y2){
+alert(xA)
+}
+
+
+
+
+function id(id){
+  return  +document.getElementById(id).value
 
 }
