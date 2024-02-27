8cc111486681e01a501c3bfffd5691f1a6919ef5
Oliver0731 <o.townell@share.epsb.ca>
2024-02-27 09:36:50 -0700

    Fixing stuff.

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-//eventlistener
-document.getElementById("btn").addEventListener("click", dist)
+document.getElementById("btn").addEventListener("click", calcP);
 
-let xA = id("xA");
-
-
-
-
-// Triangle Perimeter Assignment Start Code
+//calc dist btween points
 function dist(x1, y1, x2, y2){
-alert(xA)
-}
+   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    }
 
 
 
+    //output
+function calcP() {
+    let xA = +document.getElementById("xA").value;
+    let yA = +document.getElementById("yA").value;
+    let xB = +document.getElementById("xB").value;
+    let yB = +document.getElementById("yB").value;
+    let xC = +document.getElementById("xC").value;
+    let yC = +document.getElementById("yC").value;
 
-function id(id){
-  return  +document.getElementById(id).value
-
+let AB = dist(xA, yA, xB, yB);
+let BC = dist(xB, yB, xC, yC);
+let AC = dist(xA, yA, xC, yC);
+let perimeter = AB + BC + AC;
+document.getElementById("output").innerHTML = "Perimiter:" + Math.round(perimeter);
+    
 }
